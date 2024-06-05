164438a02225de4a59ef4320c8bf54a0c45fcc67
tamon01 <webservicecreator0421@gmail.com>
2024-06-05 13:12:11 +0900

    db test

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import ContactForm
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+from models import ContactMessage
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        message = ContactMessage(name=form.name.data, email=form.email.data, message=form.message.data)
+        db.session.add(message)
+        db.session.commit()
+        flash('Your message has been sent.')
+        return redirect(url_for('contact'))
+    return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class ContactMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<ContactMessage {self.name} - {self.email}>'
